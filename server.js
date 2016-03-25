826e9e2fa281d78a7131bd930d9ac6c429f26b48
Saket Kale <saket.kale@seedinfotech.com>
2016-03-25 11:23:01 +0530

    Initialize repo

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+app.get('/',function(req, res){
+	res.send('Hello Express!!!');
+});
+
+app.get('/about', function(req, res){
+	res.send('About us!!!');
+});
+app.listen(3000);
